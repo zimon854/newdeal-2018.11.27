221030d41e5d999643915eafb6dbb52f40c08834
zimon854 <rudfhr71@gmail.com>
2018-11-29 10:45:10 +0900

    v04

---
+++ newdeal-project-04/src/main/java/Lesson.java
@@ -0,0 +1,15 @@
+import java.sql.Date;
+
+//새 데이터 타입
+//새로운 종류의 메모리를 구성하는 설계또
+public class Lesson {
+
+	int no;
+	String title;
+	String contents;
+	Date startDate;
+	Date endDate;
+	int totalHours;
+	int dayHours;
+	//메모리를 만들라는 명령어
+}
